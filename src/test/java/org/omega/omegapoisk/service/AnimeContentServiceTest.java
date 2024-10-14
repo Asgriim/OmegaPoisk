3dbd588342f8cd626c987f1b18e3fd4db68baa5c
Aw&FaGi <my.sky12@yandex.ru>
2024-10-14 19:31:35 +0300

    add: AnimeContentServiceTest

---
+++ src/test/java/org/omega/omegapoisk/service/AnimeContentServiceTest.java
@@ -0,0 +1,69 @@
+package org.omega.omegapoisk.service;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.omega.omegapoisk.entity.content.Anime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Testcontainers
+@Transactional
+class AnimeContentServiceTest {
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:latest"
+    );
+
+    @Autowired
+    AnimeContentService animeContentService;
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void shouldGetAllAnime() {
+        animeContentService.create(new Anime(13));
+        assertThat(animeContentService.getAll()).hasSize(1);
+
+        animeContentService.create(new Anime(14));
+        assertThat(animeContentService.getAll()).hasSize(2);
+    }
+
+    @Test
+    void shouldGetAnimeById() {
+        Anime createdAnime = animeContentService.create(new Anime(13));
+        Anime retrievedAnime = animeContentService.getById(createdAnime.getId());
+
+        assertThat(retrievedAnime).isNotNull();
+        assertThat(retrievedAnime.getId()).isEqualTo(createdAnime.getId());
+        assertThat(retrievedAnime.getSeriesNum()).isEqualTo(createdAnime.getSeriesNum());
+    }
+
+
+}
