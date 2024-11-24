80d7fd2ce501d44f77b44211e2ae2f4b5363b033
Asgriim <andrew.vlasenkov70@gmail.com>
2024-11-24 22:47:40 +0300

    add: tag-service module

---
+++ tag-service/src/main/java/org/omega/tagservice/entity/Tag.java
@@ -0,0 +1,17 @@
+package org.omega.tagservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("tags")
+public class Tag {
+    @Id
+    private long id;
+    private String name;
+}
