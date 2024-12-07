83f9a9db460b01798eaa93e4ff02a28b5102dbb4
Asgriim <andrew.vlasenkov70@gmail.com>
2024-12-08 01:03:26 +0300

    remove: lab-1 src

--- src/main/java/org/omega/omegapoisk/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.omega.omegapoisk.exception;
-
-public class UserAlreadyExistsException extends RuntimeException{
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
