06ddb6a60aa5487ca27631a616a95cad27aa804a
Aw&FaGi <my.sky12@yandex.ru>
2024-12-22 19:13:35 +0300

    Fix: rating test

--- rating-service/src/test/java/org/omega/ratingservice/service/AvgRatingServiceTest.java
+++ rating-service/src/test/java/org/omega/ratingservice/service/AvgRatingServiceTest.java
@@ -3,13 +3,17 @@ package org.omega.ratingservice.service;
 import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.omega.common.core.kafka.KafkaProducerService;
+import org.omega.ratingservice.controller.RatingController;
 import org.omega.ratingservice.entity.AvgRating;
 import org.omega.ratingservice.entity.Rating;
 import org.omega.ratingservice.repository.AvgRatingRepository;
 import org.omega.ratingservice.repository.RatingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -18,7 +22,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@MockBean(RatingController.class)
+@MockBean(KafkaProducerService.class)
 @Testcontainers
 class AvgRatingServiceTest {
 
